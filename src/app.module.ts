73c7c6d6d13d71c14439203ec6865cee58813db2
Fufa Wakgari <fufawakgari174@gmail.com>
2026-03-16 15:00:30 +0300

    implement jwt strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TasksModule } from './tasks/tasks.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -38,6 +39,7 @@ import { UsersModule } from './users/users.module';
     }),
     TasksModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
